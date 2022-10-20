14dd2dba8d94601f53f514425be088ef98984765
BGD <rinaxaracav@gmail.com>
2022-10-21 02:42:21 +0300

    corrected

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function randomInteger(min, max) {
   // получить случайное число от (min-0.5) до (max+0.5)
   if (min >= 0 && max >= 0) {
-    if (max <= min) { return NaN }
+    if (max <= min) { return NaN; }
     else {
-      let rand = min - 0.5 + Math.random() * (max - min + 1);
+      const rand = min - 0.5 + Math.random() * (max - min + 1);
       return Math.round(rand);
     }
   }
@@ -12,14 +12,14 @@ function randomInteger(min, max) {
   }
 
 }
-console.log(randomInteger(2, 23));
+randomInteger(2, 23);
 
 function lengthString(str, maxLength) {
-  let stringLength = str.length;
-  if (stringLength <= maxLength && stringLength != 0) {
+  const stringLength = str.length;
+  if (stringLength <= maxLength && stringLength !== 0) {
     return true;
   }
   else { return false; }
 }
-console.log(lengthString('', 3));
+lengthString('', 3);
 
